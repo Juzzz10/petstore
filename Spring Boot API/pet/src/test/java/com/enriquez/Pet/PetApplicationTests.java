d49cadf72e7ffed8345efbc1805fd2f1bfbf06ee
Justine Enriquez <justin9953enriquez@gmail.com>
2025-05-15 16:38:25 +0800

    Add files via upload

---
+++ Spring Boot API/pet/src/test/java/com/enriquez/Pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enriquez.Pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
